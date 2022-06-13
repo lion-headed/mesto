37735f3234b7b36d2f1e7cc16b4fb0af38ff5c7d
Alexandr Yagupov <yagupovalexandr@yandex.ru>
2022-06-13 17:32:50 +0300

    hope 8

--- script/index.js
+++ script/index.js
@@ -1,17 +1,17 @@
-const formElement = document.querySelector('.popup__form');
+let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('#edit_traveler');
 let jobInput = formElement.querySelector('#edit_vocation');
 let profileTraveler = document.querySelector('.profile__traveler');
 let profileVocation = document.querySelector('.profile__vocation');
-const formOpener = document.querySelector('.profile__edit');
-const content = document.querySelector('.popup');
+let formOpener = document.querySelector('.profile__edit');
+let content = document.querySelector('.popup');
+let formCloser = document.querySelector('.popup__closebutton');
 
 function popupOpened() {
     content.classList.add('popup_opened');
     nameInput.value = profileTraveler.textContent;
-    jobInput = profileVocation.textContent;
+    jobInput.value = profileVocation.textContent;
 }
-formOpener.addEventListener('click', popupOpened);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
@@ -20,12 +20,10 @@ function formSubmitHandler (evt) {
     popupClosed();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-
-const formCloser = document.querySelector('.popup__closebutton');
-
 function popupClosed() {
     content.classList.remove('popup_opened');
 }
 
+formOpener.addEventListener('click', popupOpened);
+formElement.addEventListener('submit', formSubmitHandler);
 formCloser.addEventListener('click', popupClosed);
